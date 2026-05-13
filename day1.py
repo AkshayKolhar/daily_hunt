806659455bd4d0a6a50374587ea5c4c6743014c9
Akshay Kumar <akshaykumarkolhar@gmail.com>
2026-05-13 22:30:52 +0530

    day1 completed

---
+++ day1.py
@@ -0,0 +1,40 @@
+#day 1 (01)
+import datetime 
+date=datetime.date.today()
+time=datetime.datetime.now().time()
+
+
+def det(name,age):
+    print(f"Hello {name}, you are {age} years old!! ")
+def detials():
+    while True:
+        name=input("Enter your name: ")
+        age=int(input("enter your age(should be +ve): "))
+        if age>0:
+            det(name,age)
+            break
+        else:
+            print("Please enter a valid age!!")
+
+#day 1 (02)
+
+class bill:
+    def __init__(self,HN,units):
+        self.HN=HN
+        self.units=units
+        self.bill_cost=0
+    
+    def bil_cal(self):
+        self.bill_cost=self.units*8
+    
+    def billing(self):
+        print(f"billing on {date}\nat {time}\nHouse no: {self.HN}\nthis month:\n electricity units :{self.units}\n bill cost is : {self.bil_cal()}\nThank you")
+
+def ent_det():
+    
+    hn=int(input("enter the house no: "))
+    units=int(input("enter the electricity units: "))
+    b=bill(hn,units)
+    b.billing()
+
+ent_det()
