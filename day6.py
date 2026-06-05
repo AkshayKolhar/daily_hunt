faf3747ef941c0599febbc875a0da1c4bc12b068
Akshay Kumar <akshaykumarkolhar@gmail.com>
2026-06-05 21:06:23 +0530

    adding day6

---
+++ day6.py
@@ -0,0 +1,18 @@
+#day -> 06
+#printing patterns 
+def day61():
+    n=5
+    for i in range(1,n):
+        print("*"*i)
+
+def day62():
+    n=3
+    j=1
+    for i in range(1,n+1):
+        
+        print(" "*(n-i),"*"*j)
+        j+=2
+
+
+
+        
